5347d5d9ef01c272bdd58b9a484e7edc6baca066
worthychukwuemeka <worthy.chukwuemeka@stu.cu.edu.ng>
2023-10-25 21:54:45 +0100

    Real-time chat app; second commit

--- backend/index.js
+++ backend/index.js
@@ -7,18 +7,19 @@ app.use(express.json());
 app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
-  const { username } = req.body;
+    const { username } = req.body;
 
-  try {
-    const r = await axios.put (
-        'https://api.chatengine.io/users/',
-        {username: username, secret: username, first_name: username},
-        {headers: {"private-key":"e1892932-36ac-489b-a103-30a51a1ccda5"}}
-    )
-  } catch (e) {
-
-  }
-  return res.json({ username: username, secret: "sha256..." });
+    try {
+        const r = await axios.put(
+            'https://api.chatengine.io/users/',
+            { username: username, secret: username, first_name: username },
+            { headers: { "private-key": "e1892932-36ac-489b-a103-30a51a1ccda5" } }
+        )
+        return res.status(r.status).json(r.data)
+    } catch (e) {
+        return res.status(e.response.status).json(e.response.data);
+    }
+    return res.json({ username: username, secret: "sha256..." });
 });
 
 app.listen(3001);
